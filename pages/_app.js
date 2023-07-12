394897732acb9fa8760c4bdc79270ddfb58e3bf6
Sungbin Jo <goranmoomin@daum.net>
2023-07-12 13:22:19 +0900

    Add simple password page

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,23 @@
 import "../styles/app.sass";
 
+import { useState } from "react";
+import { Box, Form } from "react-bulma-components";
+
 function MyApp({ Component, pageProps }) {
+    let [password, setPassword] = useState("");
+    let isAuthorized = password == "susaekns";
+    if (!isAuthorized) {
+        return (
+            <Box style={{ maxWidth: 240, margin: "auto" }}>
+                <Form.Field>
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control>
+                        <Form.Input placeholder="password" type="password" value={password} onChange={e => setPassword(e.target.value)} />
+                    </Form.Control>
+                </Form.Field>
+            </Box>
+        );
+    }
     return <Component {...pageProps} />;
 }
 
