bb6ba3828cad4bc964e9643924aa3d117f1e42a4
Sungbin Jo <goranmoomin@daum.net>
2023-07-13 18:46:17 +0900

    Add copyright footer

--- components/layout.js
+++ components/layout.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { Hero, Container } from "react-bulma-components";
+import { Hero, Container, Footer, Content } from "react-bulma-components";
 
 import Head from "./head.js";
 import Navbar from "./navbar.js";
@@ -20,6 +20,16 @@ export default function Layout({ header, children }) {
             <main>
                 <Container>{children}</Container>
             </main>
+            <footer>
+                <Footer>
+                    <Container>
+                        <Content style={{ textAlign: "center" }}>
+                            <strong>수색신경외과의원</strong> 서울특별시 은평구 수색로 256, 3층 (수색동 106-1)<br />
+                            <strong>TEL.</strong> 02-305-4463 <strong>사업자번호.</strong> 110-91-59844
+                        </Content>
+                    </Container>
+                </Footer>
+            </footer>
         </>
     );
 }
